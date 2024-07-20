e666fb9c21af8ae3622854928a02350fd729f8cd
Quang Trong <quangtrong1506@gmail.com>
2024-07-20 11:11:18 +0700

    refactor(Component): :recycle: save temp

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,8 +1,8 @@
-
-const APP_NAME = "Ốc Sa Đoạ"
-
+const APP_NAME = 'Ốc Sa Đoạ';
+const APP_DESCRIPTION = 'Website chia sẻ tài liệu nghiên cứu khoa học hàng đầu thế giới';
 const BuildConfig = {
   APP_NAME,
-}
+  APP_DESCRIPTION,
+};
 
-export {BuildConfig}
+export { BuildConfig };
