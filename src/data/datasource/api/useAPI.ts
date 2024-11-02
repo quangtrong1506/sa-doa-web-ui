8202962674a580034bba8e227ed3000413a74e47
Quang Trong <quangtrong1506@gmail.com>
2024-11-02 18:03:22 +0700

    refactor: :recycle: test

--- src/data/datasource/api/useAPI.ts
+++ src/data/datasource/api/useAPI.ts
@@ -1,18 +1,36 @@
-// src/utils/UserAPI.ts
 import BaseAPI from './baseApi';
 
 export type User = {
    id: string;
    name: string;
    email: string;
-   // Thêm các trường khác nếu cần
 };
 
-const userAPI = new BaseAPI<User>('user', {
-   baseURL: 'https://sa-doa-be.vercel.app',
-   headers: {
-      Authorization: 'Bearer YOUR_TOKEN',
-   },
-});
+class UserAPI extends BaseAPI<User> {
+   constructor() {
+      super('user', {
+         baseURL: 'https://sa-doa-be.vercel.app',
+      });
+   }
 
+   // Hàm để tạo người dùng mới
+   async createUser(data: Partial<User>, token: string): Promise<User> {
+      return this._post<User>('/', data, {
+         headers: {
+            Authorization: `Bearer ${token}`,
+         },
+      });
+   }
+
+   // Hàm để cập nhật người dùng theo ID
+   async updateUser(id: string, data: Partial<User>, token: string): Promise<User> {
+      return this._put<User>(`/${id}`, data, {
+         headers: {
+            Authorization: `Bearer ${token}`,
+         },
+      });
+   }
+}
+
+const userAPI = new UserAPI();
 export { userAPI };
